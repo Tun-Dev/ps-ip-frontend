a1238a1febf2a6ef62f7ca8d2cfc2e0365d3b857
dewaley <adewaleademilua@gmail.com>
2024-09-28 10:40:41 +0100

    button restructure

--- src/shared/chakra/components/index.ts
+++ src/shared/chakra/components/index.ts
@@ -4,12 +4,14 @@ import Heading from './heading';
 import Input from './input';
 import Select from './select';
 import Text from './text';
+import buttonTheme from "./buttonTheme";
 
 const components: ThemeComponents = {
   Input: Input,
   Select: Select,
   Text: Text,
   Heading: Heading,
+  Button: buttonTheme,
 };
 
 export default components;
