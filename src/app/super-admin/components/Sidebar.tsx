a6d0cb902969a2c6241fc06546c52b33ddb8bcb0
Douye Tungbulu <tungbulupaul@gmail.com>
2024-10-05 00:40:03 +0100

    updated sidebar

--- src/app/super-admin/components/Sidebar.tsx
+++ src/app/super-admin/components/Sidebar.tsx
@@ -1,7 +1,17 @@
 'use client';
 
 import { Flex, Image, Text } from '@chakra-ui/react';
-import { MdGroups, MdHome, MdLocalShipping, MdNoteAlt, MdViewCarousel, MdVolunteerActivism } from 'react-icons/md';
+import {
+  MdGroups,
+  MdHome,
+  MdLocalShipping,
+  MdLogout,
+  MdNoteAlt,
+  MdNotifications,
+  MdPerson,
+  MdViewCarousel,
+  MdVolunteerActivism,
+} from 'react-icons/md';
 import { useRouter, usePathname } from 'next/navigation';
 
 const sideBarData = [
@@ -25,7 +35,39 @@ const Sidebar = () => {
         ))}
       </Flex>
 
-      <Flex justifySelf="end">Bottom</Flex>
+      <Flex flexDir="column">
+        <Flex alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center" gap="15px">
+            <MdNotifications size="20px" color="#077D00" />
+            <Text variant="Body2Semibold" color="#077D00">
+              Notifications
+            </Text>
+          </Flex>
+          <Flex boxSize="15px" borderRadius="50%" bg="red" alignItems="center" justifyContent="center">
+            <Text variant="Body3Semibold" color="white">
+              3
+            </Text>
+          </Flex>
+        </Flex>
+
+        <Flex flexDir="column" mt="88px" gap="16px">
+          <Flex h="64px" borderRadius="6px" bg="primary.100" alignItems="center" justifyContent="center" gap="8px">
+            <Flex boxSize="40px" bg="white" borderRadius="16px" justifyContent="center" alignItems="center">
+              <MdPerson size="24px" color="#9CCB99" />
+            </Flex>
+            <Flex flexDir="column">
+              <Text variant="Body2Bold">Chukwuemeka Aliu</Text>
+              <Text variant="Body3Semibold">ChukwuAliu@gmail.com</Text>
+            </Flex>
+          </Flex>
+          <Flex gap="8px" alignItems="center" justifyContent="center" cursor="pointer" onClick={() => {}}>
+            <MdLogout size="16px" color="red" />
+            <Text variant="Body1Bold" color="red">
+              Log out
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
