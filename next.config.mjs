3d9ed397b53223f6b17766deaf368baf58e694e5
Douye Tungbulu <tungbulupaul@gmail.com>
2025-02-24 10:34:16 +0100

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: { remotePatterns: [{ protocol: 'https', hostname: 'boi-ip-is.s3.eu-north-1.amazonaws.com' }] },
+};
 
 export default nextConfig;
