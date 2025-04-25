02435b9a3f736f575da3a356166d2f87718efe5a
Douye Tungbulu <tungbulupaul@gmail.com>
2025-04-25 02:48:00 +0100

    update

--- src/shared/chakra/components/QueryWrapper.tsx
+++ src/shared/chakra/components/QueryWrapper.tsx
@@ -10,6 +10,8 @@ const createQueryClient = () => {
     defaultOptions: {
       queries: {
         staleTime: 5 * 60 * 1000, // 5 minutes
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
       },
     },
   });
